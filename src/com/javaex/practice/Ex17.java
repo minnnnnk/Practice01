d063d0f10de87582c241ddda2265b9bb386786a8
mingyu <jubal9153@gmail.com>
2022-04-15 09:23:05 +0900

    Ex17 complete

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Ex17 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-				
+		
 		System.out.print("반지름: ");	
 		int radius = sc.nextInt();
-		System.out.println("구의부피는: " + (4/3)*3.14*(radius*radius*radius));
-				
+		double pi = 3.14;
+		
+		System.out.println("구의부피는: " +(4.0/3.0)*(radius*radius*radius)*pi);
+		
 				
 		sc.close();
 	}
